d29c2385fd05cf546d9c219aba5096bd38d85117
nlumiere <nicholaslumiere@gmail.com>
2023-09-11 12:59:24 -0700

    basic python openai endpoint for hard-coded prompt

--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 import os
+import openai
 from sys import exit
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -7,11 +8,21 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 OPENAI_KEY = os.environ.get("OPENAI_KEY")
-OPENAI_ENDPOINT = "https://emeaopenai.azure-api.net"
 OPENAI_MODEL = os.environ.get("OPENAI_MODEL")
 
-if OPENAI_KEY and OPENAI_ENDPOINT and OPENAI_MODEL:
+if OPENAI_KEY and OPENAI_MODEL:
+    try:
+        openai.api_key = OPENAI_KEY
+    except:
+        print("NO :(")
     print("Keys loaded successfully!")
 else:
     print("Error in setting up the environment")
     exit()
+
+def main():
+    chat_completion = openai.ChatCompletion.create(model=OPENAI_MODEL, messages=[{"role": "user", "content": "Hello world"}])
+    print(chat_completion)
+
+if __name__ == "__main__":
+    main()
