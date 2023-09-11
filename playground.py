68c085fe4a788901f4d6484b15fa119c1585092a
nlumiere <nicholaslumiere@gmail.com>
2023-09-11 12:17:39 -0700

    adds endpoint and model

--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
 import os
+from sys import exit
 from os.path import join, dirname
 from dotenv import load_dotenv
 
@@ -6,4 +7,11 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 OPENAI_KEY = os.environ.get("OPENAI_KEY")
-print(OPENAI_KEY)
+OPENAI_ENDPOINT = "https://emeaopenai.azure-api.net"
+OPENAI_MODEL = os.environ.get("OPENAI_MODEL")
+
+if OPENAI_KEY and OPENAI_ENDPOINT and OPENAI_MODEL:
+    print("Keys loaded successfully!")
+else:
+    print("Error in setting up the environment")
+    exit()
