f3e903396f77ffbbd55c882e26e1ee9a21bed862
Crystal Chong <crystalchong@microsoft.com>
2023-09-12 15:38:05 -0700

    Created json file with spoiler labels for titles

--- utils/checkTitleSpoiler.py
+++ utils/checkTitleSpoiler.py
@@ -1,14 +1,25 @@
-import os, sys
+import os, sys, json
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import playground
-# Using readlines()
+
 titles = []
 with open('data/titles.txt', encoding="utf8") as f:
     for line in f:
         if not line.startswith("==="):
             titles.append(line)
 
-print(titles)
-
+dictionary = {}
 for title in titles:
     response = playground.request(title)
+    if "No" in response:
+        dictionary[title] = "No"
+    else:
+        dictionary[title] = "Yes"
+
+
+# Serializing json
+json_object = json.dumps(dictionary, indent=4)
+ 
+# Writing to sample.json
+with open("data/labeledTitles_spoilers.txt", "a", encoding="utf-8") as f:
+    f.write(json_object)
