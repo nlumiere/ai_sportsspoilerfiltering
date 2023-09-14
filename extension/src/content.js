1ce887dc3d3b646608febf550def6369a84a328f
nlumiere <nicholaslumiere@gmail.com>
2023-09-14 15:30:45 -0700

    it work now lol

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,50 +1,91 @@
 const titleMap = new Map();
 const thumbnailMap = new Map();
 
+function findTitle(item, query) {
+  const title = item.querySelector(query);
+  if (title && title.textContent) {
+    const text = title.textContent;
+    if (titleMap.has(text)) {
+
+    }
+    else {
+      titleMap.set(text, true);
+      // console.log("NEW VID:", text);
+      chrome.runtime.sendMessage({ title: text }, function (response) {
+        if(response){
+          console.log(response)
+        }
+        else{
+          console.log("no response")
+        }
+        // const containsSpoilerContent = response["title"]; // get from GPT
+        // if (containsSpoilerContent && response["alt_title"] != "") {
+        //   try {
+        //     title.textContent = response["alt_title"];
+        //     console.log("Spoiler title:", text, "; New title:", response["alt_title"]);
+        //   }
+        //   catch {
+        //     console.log("DOM not fully loaded");
+        //   }
+        // }
+      });
+    }
+  }
+}
+
 function filterVideos() {
   const richItems = document.querySelectorAll("ytd-rich-item-renderer");
   richItems.forEach((item) => {
-    const title = item.querySelector("yt-formatted-string#video-title");
-    if (title && title.textContent) {
-      const text = title.textContent;
-      if (titleMap.has(text)) {
-
-      }
-      else {
-        // TODO call GPT
-        console.log(text);
-        const containsSpoilerContent = true; // get from GPT
-        titleMap.set(text, containsSpoilerContent);
-      }
-    }
-    const thumbnail = item.querySelector("yt-image.ytd-thumbnail");
-    if (thumbnail) {
-      const thumbnailImage = thumbnail.querySelector("img");
-      if (thumbnailImage && thumbnailImage.src) {
-        const url = thumbnailImage.src;
-        if (thumbnailMap.has(url)) {
-          // Do nothing (?)
-        }
-        else {
-          // TODO get image bytes and call cnn
-          chrome.runtime.sendMessage({ thumbnail: url }, function (response) {
-            console.log("Contains spoiler content", response);
-          });
-          const containsSpoilerContent = true; // get from CNN
-          thumbnailMap.set(url, containsSpoilerContent);
-        }
-      }
-    }
+    findTitle(item, "yt-formatted-string#video-title");
   });
+  const richItems2 = document.querySelectorAll("ytd-video-renderer");
+  richItems2.forEach((item) => {
+    findTitle(item, "yt-formatted-string.ytd-video-renderer");
+  });
+    // const thumbnail = item.querySelector("yt-image.ytd-thumbnail");
+    // if (thumbnail) {
+    //   const thumbnailImage = thumbnail.querySelector("img");
+    //   if (thumbnailImage && thumbnailImage.src) {
+    //     const url = thumbnailImage.src;
+    //     if (thumbnailMap.has(url)) {
+    //       // Do nothing (?)
+    //     }
+    //     else {
+    //       thumbnailMap.set(url, true);
+    //       // TODO get image bytes and call cnn
+    //       chrome.runtime.sendMessage({ thumbnail: url }, function (response) {
+    //         // console.log("Contains spoiler content", response);
+    //       });
+    //       const containsSpoilerContent = true; // get from CNN
+    //     }
+    //   }
+    // }
 }
 
-const observer = new MutationObserver(filterVideos);
-observer.observe(document.body, { childList: true, subtree: true });
+function handleMutations(mutationsList, observer) {
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      // Check if any "ytd-rich-item-renderer" elements were added
+      const addedNodes = mutation.addedNodes;
+      for (const node of addedNodes) {
+        if (node instanceof HTMLElement && node.matches('ytd-rich-item-renderer')) {
+          findTitle(node, "yt-formatted-string#video-title");
+        }
+        else if (node instanceof HTMLElement && node.matches('ytd-video-renderer')) {
+          findTitle(node, "yt-formatted-string.ytd-video-renderer");
+        }
+      }
+    }
+  }
+}
 
 window.onload = () => {
   filterVideos();
 }
 
-chrome.runtime.sendMessage({ greeting: "Hello from content script!" }, function (response) {
-  console.log("Received response from background:", response);
-});
+const observer = new MutationObserver(handleMutations);
+observer.observe(document.body, { childList: true, subtree: true });
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log("yeet.");
+})
--- extension/src/service_worker.js
+++ extension/src/service_worker.js
@@ -1,13 +1,91 @@
 // import * as tf from "@tensorflow/tfjs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const OPENAI_KEY = process.env.OPENAI_KEY;
 
 chrome.runtime.onInstalled.addListener(() => {
     console.log('Extension installed');
 });
 
+const getDoesContainSpoilerContentFromTitle = async (title) => {
+	const response = await makeGPTRequestFromTitle(title);
+	// if "yes" is a substring of the response
+	if (typeof response === "string" && response.toLowerCase().indexOf("yes") !== -1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+const makeGPTRequestFromTitle = async (title) => {
+	const content = "If this is a sports event, tell me in one word if the following title contains spoiler information for the sports event: \"" + title;
+	const response = await makeGPTRequest(content);
+	return response;
+}
+
+const makeGPTRequest = async (content) => {
+	const headers = {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${OPENAI_KEY}`,
+	};
+	const requestBody = {
+		"model": "gpt-3.5-turbo",
+		"messages": [{"role": "user", "content": content}],
+		"temperature": 0.7
+	}
+	const response = await fetch("https://api.openai.com/v1/chat/completions", {
+		method: 'POST',
+		headers: headers,
+		body: JSON.stringify(requestBody),
+	});
+	const data = await response.json();
+	return data.choices[0].message.content;
+}
+
+const getAltTitleFromTitle = async (title) => {
+	const followupPrompt = "What would an alternate title for: \"" + title + "\" that contains no spoiler information about the event?";
+	const headers = {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${OPENAI_KEY}`,
+	};
+	const requestBody = {
+		"model": "gpt-3.5-turbo",
+		"messages": [{"role": "user", "content": followupPrompt}],
+		"temperature": 0.7
+	}
+	const response = await fetch("https://api.openai.com/v1/chat/completions", {
+		method: 'POST',
+		headers: headers,
+		body: JSON.stringify(requestBody),
+	});
+	const data = await response.json();
+	return data.choices[0].message.content;
+}
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log("Received message from content script:", message);
-    if (Object.keys(message).includes("thumbnail")) {
-        console.log(message["thumbnail"]);
-    }
-    sendResponse({ farewell: "Goodbye from background script!" });
+	trySendResponseAsync(message, sender, sendResponse)
+	return true;
 });
+
+async function trySendResponseAsync(message, sender, sendResponse){
+	const response = {
+		"thumbnail": false,
+		"title": false,
+		"alt_title": ""
+	}
+	if (Object.keys(message).includes("thumbnail")) {
+		// TODO:
+	}
+	else if (Object.keys(message).includes("title")) {
+		const containsSpoilers = await getDoesContainSpoilerContentFromTitle(message["title"]);
+		response["title"] = containsSpoilers
+		console.log(message["title"], containsSpoilers);
+	}
+	if (response["title"]) {
+		response["alt_title"] = await getAltTitleFromTitle(message["title"]);
+	}
+	console.log(response);
+	sendResponse({ doesContainSpoilers: response });
+}
